c2d04d45520ca2a235b91cbc768f6766ff87c552
Volker Lanz <vl@fidra.de>
2009-01-10 09:09:47 +0000

    Remove scripts dir from tarballs.
    Remove original CMakeLists.txt after patching it.
    
    svn path=/trunk/extragear/sysadmin/partitionmanager/; revision=908701

--- scripts/partitionmanagerapp.rb
+++ scripts/partitionmanagerapp.rb
@@ -28,33 +28,38 @@ class PartitionManagerApp < Application
 	end
 
 	def applyFixes(workingDir, outputDir)
-		puts "applying fixes for partition manager"
+		puts 'applying fixes for partition manager'
  
 		Dir.chdir "#{workingDir}/#{outputDir}"
 
-		return if !File.exists? 'doc'
+		puts 'removing scripts subdir...'
+		FileUtils.rm_rf 'scripts'
 
-		FileUtils.mv('CMakeLists.txt', 'CMakeLists.txt.orig')
+		if File.exists? 'doc' then
+			puts 'patching CMakeLists.txt to build docs only with new kdelibs...'
+			
+			FileUtils.mv('CMakeLists.txt', 'CMakeLists.txt.orig')
 
-		newFile = File.new('CMakeLists.txt', File::CREAT | File::TRUNC | File::RDWR)
+			newFile = File.new('CMakeLists.txt', File::CREAT | File::TRUNC | File::RDWR)
 
-		IO.foreach('CMakeLists.txt.orig')  do |line|
-			if line =~ /\s*macro_optional_add_subdirectory\s*\(\s*doc\s*\)\s*/i
-				newFile.print <<END_OF_TEXT
+			IO.foreach('CMakeLists.txt.orig')  do |line|
+				if line =~ /\s*macro_optional_add_subdirectory\s*\(\s*doc\s*\)\s*/i
+					newFile.print <<END_OF_TEXT
 if (KDE_VERSION VERSION_GREATER "4.1.3")
-    macro_optional_add_subdirectory(doc)
+	macro_optional_add_subdirectory(doc)
 else (KDE_VERSION VERSION_GREATER "4.1.3")
-    message (STATUS "No documentation built for KDE libs < 4.1.4")
+	message (STATUS "No documentation built for KDE libs < 4.1.4")
 endif (KDE_VERSION VERSION_GREATER "4.1.3")
 END_OF_TEXT
-			else
-				newFile << line
+				else
+					newFile << line
+				end
 			end
+
+			FileUtils.rm_rf 'CMakeLists.txt.orig' 
+
+			newFile.close
 		end
-
-		FileUtils.rm_rf 'CMakeLists.txt.orig' 
-
-		newFile.close
 	end
 end
 
