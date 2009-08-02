079f24cc6d4f0e0f1bdd2068805d1e6bbda73bda
Volker Lanz <vl@fidra.de>
2009-08-02 18:44:30 +0000

    no comma after i18n context or it won't work.
    svn path=/trunk/extragear/sysadmin/partitionmanager/; revision=1005974

--- src/jobs/job.cpp
+++ src/jobs/job.cpp
@@ -202,7 +202,7 @@ bool Job::copyBlocks(Report& report, CopyTarget& target, CopySource& source)
 
 	free(buffer);
 
- 	report.line() << i18ncp("@info/plain, argument 2 is a string such as 7 sectors (localized accordingly)", "Copying 1 block (%2) finished.", "Copying %1 blocks (%2) finished.", blocksCopied, i18np("1 sector", "%1 sectors", target.sectorsWritten()));
+ 	report.line() << i18ncp("@info/plain argument 2 is a string such as 7 sectors (localized accordingly)", "Copying 1 block (%2) finished.", "Copying %1 blocks (%2) finished.", blocksCopied, i18np("1 sector", "%1 sectors", target.sectorsWritten()));
 
 	return rval;
 }
