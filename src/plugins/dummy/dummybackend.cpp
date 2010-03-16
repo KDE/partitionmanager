f3c74efa53d44ffd2ba752c3894612706d5cd140
Volker Lanz <vl@fidra.de>
2010-03-16 10:51:08 +0000

    make the dummy device smaller so resizing isn't as difficult
    svn path=/trunk/extragear/sysadmin/partitionmanager/; revision=1103941

--- src/plugins/dummy/dummybackend.cpp
+++ src/plugins/dummy/dummybackend.cpp
@@ -76,7 +76,7 @@ QList<Device*> DummyBackend::scanDevices()
 
 Device* DummyBackend::scanDevice(const QString& device_node)
 {
-	Device* d = new Device("Dummy Device", QString("/tmp" + device_node), 255, 0xffff, 63, 512);
+	Device* d = new Device("Dummy Device", QString("/tmp" + device_node), 255, 3000, 63, 512);
 	CoreBackend::setPartitionTableForDevice(*d, new PartitionTable(PartitionTable::msdos_sectorbased, 2048, d->totalSectors() - 2048));
 	CoreBackend::setPartitionTableMaxPrimaries(*d->partitionTable(), 128);
 	d->partitionTable()->updateUnallocated(*d);
