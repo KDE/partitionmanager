f8e81095eabc1594cb4f1b47c8abc31a03ae9462
Volker Lanz <vl@fidra.de>
2010-04-27 11:41:35 +0000

    add a formatted spin box
    svn path=/trunk/extragear/sysadmin/partitionmanager/; revision=1119557

---
+++ src/gui/formattedspinbox.cpp
@@ -0,0 +1,33 @@
+/***************************************************************************
+ *   Copyright (C) 2010 by Volker Lanz <vl@fidra.de>                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA            *
+ ***************************************************************************/
+
+#include "gui/formattedspinbox.h"
+
+#include <kglobal.h>
+#include <klocale.h>
+
+QString FormattedSpinBox::textFromValue(double value) const
+{
+	return KGlobal::locale()->formatNumber(value, decimals());
+}
+
+double FormattedSpinBox::valueFromText(const QString& text) const
+{
+	return KGlobal::locale()->readNumber(text);
+}
