9b33918f08393460fba827c1e738991b4ea5ea71
Volker Lanz <vl@fidra.de>
2012-11-05 10:19:27 +0000

    Remove wrong todo: The resize dialog does indeed have a dirty bit.
    svn path=/trunk/extragear/sysadmin/partitionmanager/; revision=1324098

--- src/gui/resizedialog.h
+++ src/gui/resizedialog.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2008,2010 by Volker Lanz <vl@fidra.de>                  *
+ *   Copyright (C) 2008,2010,2012 Volker Lanz <vl@fidra.de>                *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -29,10 +29,6 @@ class Partition;
 class Device;
 
 /** Let the user resize or move a Partition.
-
-	@todo The ResizeDialog does not yet have a "dirty bit".
-	It should get one and disable its OK button as long as nothing has been
-	modified, much like the PartPropsDialog already does.
 
 	@author Volker Lanz <vl@fidra.de>
 */
