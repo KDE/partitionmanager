9c91e25e96e6e393203cd5b1e97f10eb1465b93a
Andrius Štikonas <andrius@stikonas.eu>
2016-03-02 18:33:40 +0000

    Update copyright.

--- src/gui/editmountoptionsdialog.h
+++ src/gui/editmountoptionsdialog.h
@@ -1,5 +1,6 @@
 /*************************************************************************
  *  Copyright (C) 2009, 2010 by Volker Lanz <vl@fidra.de>                *
+ *  Copyright (C) 2016 by Andrius Štikonas <andrius@stikonas.eu>         *
  *                                                                       *
  *  This program is free software; you can redistribute it and/or        *
  *  modify it under the terms of the GNU General Public License as       *
--- src/gui/editmountpointdialog.cpp
+++ src/gui/editmountpointdialog.cpp
@@ -1,6 +1,7 @@
 /*************************************************************************
  *  Copyright (C) 2009, 2010 by Volker Lanz <vl@fidra.de>                *
  *  Copyright (C) 2015 by Teo Mrnjavac <teo@kde.org>                     *
+ *  Copyright (C) 2016 by Andrius Štikonas <andrius@stikonas.eu>         *
  *                                                                       *
  *  This program is free software; you can redistribute it and/or        *
  *  modify it under the terms of the GNU General Public License as       *
