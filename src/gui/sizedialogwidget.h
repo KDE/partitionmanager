13031913f4a39fa04e1372aa52329093c92c2e52
Teo Mrnjavac <teo@kde.org>
2016-04-15 15:45:22 +0200

    Update copyrights.

--- src/gui/sizedialogwidget.h
+++ src/gui/sizedialogwidget.h
@@ -1,5 +1,6 @@
 /*************************************************************************
  *  Copyright (C) 2008, 2010 by Volker Lanz <vl@fidra.de>                *
+ *  Copyright (C) 2016 by Teo Mrnjavac <teo@kde.org>                     *
  *                                                                       *
  *  This program is free software; you can redistribute it and/or        *
  *  modify it under the terms of the GNU General Public License as       *
